c579507f66ee8ecb75c7bce99211cda0eba93070
Your Name <you@example.com>
2021-03-16 00:08:59 +0700

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_5951071041_Vo_Sy_Kha.WebApiApplication" Language="C#" %>
---
+++ Models/WeatherInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WeatherController.Models
+{
+    [DataContract]
+    public class WeatherInfo
+    {
+        [DataMember(Name ="location")]
+        public string Location { get; set; }
+        [DataMember(Name ="degree")]
+        public float Degree { get; set; }
+        [DataMember(Name = "dateTime")]
+        public DateTime DataTime { get; set; }
+    }
+}
